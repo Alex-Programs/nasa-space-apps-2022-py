7c17daa973d02767cf0ff910c1158a81f8ec8500
AlexAndHisScripts <alexcj2802@gmail.com>
2022-10-01 16:38:17 +0100

    Solarstatus, sunnysideup

--- sunnysideup.py
+++ sunnysideup.py
@@ -4,8 +4,8 @@ def sunny_side_up(lat, long):
     temperature = sdo.get_temperature(lat, long)
     energy_transfer = ((temperature-273)*1889.1)
     cooking_time_seconds = ((199.2*38)/energy_transfer)
-    cooking_time_micros = cooking_time_seconds*1000000
-    return(cooking_time_micros)
+    cooking_time_millis = cooking_time_seconds*1000
+    return(cooking_time_millis)
 
 if __name__ == "__main__":
     print(sunny_side_up(0, 0))
